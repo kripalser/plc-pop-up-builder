15df3756adca285655082110e2526be51fe5954f
Pavel Krimont <kripalser@ya.ru>
2026-02-26 23:06:19 +0100

    Upgrade packages, silence Sass deprecation warnings

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
     base: '/plc-pop-up-builder/',
     plugins: [svelte()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // Silencing these deprecation warnings because Bootstrap 5 won't be compatible with Dart Sass
+                // https://github.com/twbs/bootstrap/issues/40962#issuecomment-3730409408
+                silenceDeprecations: ['color-functions', 'global-builtin', 'import', 'if-function']
+            },
+        }
+    },
 });
